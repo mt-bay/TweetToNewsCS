40e4681e755ca50b81b6bfeded53bdb860fba883
mt-bay <mountainbay89@gmail.com>
2017-03-28 22:00:40 +0900

    とりあえずjson解析の方法はわかった

--- TweetToNewsCS/Model/Domain/Tweet.cs
+++ TweetToNewsCS/Model/Domain/Tweet.cs
@@ -3,11 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Globalization;
 
 namespace TweetToNewsCS.Model.Domain
 {
     class Tweet
     {
-        string 
+        [JsonProperty("text")]
+        public string Text { get; set; }
+        [JsonProperty("geo")]
+        public string Geo { get; set; }
+        [JsonIgnore]
+        public DateTime CreatedAt => DateTime.ParseExact(createdAt, "ddd MMM dd HH:mm:ss zzz yyyy", DateTimeFormatInfo.InvariantInfo, DateTimeStyles.AdjustToUniversal);
+        [JsonProperty("created_at")]
+        private string createdAt { get; set; }
+
+
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this, Formatting.Indented);
+        }
     }
 }
--- TweetToNewsCS/Program.cs
+++ TweetToNewsCS/Program.cs
@@ -4,7 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NMeCab;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 
+using TweetToNewsCS.Model.Domain;
+using System.IO;
 
 namespace TweetToNewsCS
 {
@@ -12,7 +16,27 @@ namespace TweetToNewsCS
     {
         static void Main(string[] args)
         {
+//            try
+//            {
+                string rawJson = "";
+                using (StreamReader reader = new StreamReader("C:/src/TweetsToNewsCS/TweetToNewsCS/data/json/20160627_niigata.json"))
+                {
+                    rawJson = reader.ReadToEnd();
+                }
 
+            IsoDateTimeConverter formatter = new IsoDateTimeConverter { DateTimeFormat = @"ddd MMM dd HH:mm:ss zzz yyyy" };
+
+                List<Tweet> result = JsonConvert.DeserializeObject<List<Tweet>>(rawJson, formatter);
+
+                Console.WriteLine("resultの長さ : {0}", result.Count);
+                Console.WriteLine("resultのサンプル(時間 : {0}) : {1}", result[0].CreatedAt, result[0]);
+//            }
+//            catch(Exception e)
+//            {
+//                Console.WriteLine(e.Message);
+//            }
+            
+            Console.ReadKey();
         }
     }
 }
