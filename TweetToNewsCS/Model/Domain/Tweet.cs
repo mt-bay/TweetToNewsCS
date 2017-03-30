8cb8510921c249cd1d5afa9f44877e48d68f64b9
mt-bay <mountainbay89@gmail.com>
2017-03-30 21:03:46 +0900

    WebAPIを叩くクラス作成、Getのみ実装　テストまだです

--- TweetToNewsCS/Model/Domain/Tweet.cs
+++ TweetToNewsCS/Model/Domain/Tweet.cs
@@ -8,18 +8,39 @@ using System.Globalization;
 
 namespace TweetToNewsCS.Model.Domain
 {
+    /// <summary>
+    /// ツイートを保存するクラス
+    /// </summary>
     class Tweet
     {
+        /// <summary>
+        /// ツイート記事内容
+        /// </summary>
         [JsonProperty("text")]
         public string Text { get; set; }
+
+        /// <summary>
+        /// 位置情報。なければnull
+        /// </summary>
         [JsonProperty("geo")]
         public string Geo { get; set; }
+
+        /// <summary>
+        /// 投稿日時
+        /// </summary>
         [JsonIgnore]
         public DateTime CreatedAt => DateTime.ParseExact(createdAt, "ddd MMM dd HH:mm:ss zzz yyyy", DateTimeFormatInfo.InvariantInfo, DateTimeStyles.AdjustToUniversal);
+        /// <summary>
+        /// 投稿日時(APIで取得したそのままのデータ)
+        /// </summary>
         [JsonProperty("created_at")]
         private string createdAt { get; set; }
 
 
+        /// <summary>
+        /// インスタンス情報を文字列化して返す
+        /// </summary>
+        /// <returns>文字列化したインスタンス情報</returns>
         public override string ToString()
         {
             return JsonConvert.SerializeObject(this, Formatting.Indented);
---
+++ TweetToNewsCS/Model/Infrastructure/Api.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+
+namespace TweetToNewsCS.Model.Infrastructure
+{
+    /// <summary>
+    /// WebAPIを叩くクラス
+    /// </summary>
+    static class Api
+    {
+        /// <summary>
+        /// GET
+        /// </summary>
+        /// <param name="url">URL</param>
+        /// <param name="query">クエリ(hoge=piyoの形式で書いてください)</param>
+        /// <returns>受け取った結果</returns>
+        /// <exception cref="WebException">正常処理以外のステータスが帰ってきた場合</exception>
+        public static string Get(string url, params string[] query)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url + ArgsToQuery(query));
+            request.Method = "Get";
+
+            return Access(request);
+        }
+
+
+        /// <summary>
+        /// 引数のHttpWebRequestを用いてAPIコールし、結果を取得する
+        /// </summary>
+        /// <param name="request">アクセス先のHttpWebRequest</param>
+        /// <returns>受け取った結果</returns>
+        /// /// <exception cref="WebException">正常処理以外のステータスが帰ってきた場合</exception>
+        private static string Access(HttpWebRequest request)
+        {
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            {
+                if (!IsSuccessStatusCode((int)response.StatusCode))
+                {
+                    throw new WebException(request.RequestUri.ToString() + "の取得に失敗しました(" + response.StatusDescription + ")");
+                }
+
+                using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+                {
+                    return reader.ReadToEnd();
+                }
+            }
+        }
+        
+
+        private static bool IsSuccessStatusCode(int statusCode)
+        {
+            return 200 <= statusCode && statusCode < 300;
+        }
+
+
+        /// <summary>
+        /// 与えられた引数をパラメータ化して返す
+        /// </summary>
+        /// <param name="query">パラメータ化したい内容</param>
+        /// <returns>引数をパラメータ化したもの</returns>
+        private static string ArgsToQuery(params string[] query)
+        {
+            return query.Any() ? "?" + string.Join("&", query) : string.Empty;
+        }
+    }
+}
